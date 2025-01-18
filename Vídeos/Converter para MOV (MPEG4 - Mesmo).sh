45ca3914c12d9e4f1f846d837435b29b8936aa3a
Fernando <33039864+tuxslack@users.noreply.github.com>
2025-01-17 23:25:41 -0300

    Update Converter para MOV (MPEG4 - Mesmo).sh

--- Vídeos/Converter para MOV (MPEG4 - Mesmo).sh
+++ Vídeos/Converter para MOV (MPEG4 - Mesmo).sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
+# Colaboração:    Fernando Souza - https://www.youtube.com/@fernandosuporte/
+
+clear
+
+# -------------------------------------------------------------------------------------------------
+
+# Verificar se os programas estão instalados:
+
+which sed         1> /dev/null || exit 1
+which ffmpeg      1> /dev/null || exit 2
+which notify-send 1> /dev/null || exit 3
+
+
+# -------------------------------------------------------------------------------------------------
+
 {
 
 readarray FILENAME <<< "$(echo -e "$NAUTILUS_SCRIPT_SELECTED_FILE_PATHS" | sed -e 's/\r//g')"
 
 for file in "${FILENAME[@]}"; do
+
     file=$(echo "$file" | tr -d $'\n')
+    
     ffmpeg -i "$file" -c:v mpeg4 -map 0 -q:v 0 -codec:a pcm_s16le "${file%.*}-converted.mov"
+    
 done
 
-notify-send "Deu certo!" "Os arquivos foram convertidos para MOV com sucesso!" --app-name="Transform" --icon="/home/raulcraveiro/.local/share/icons/custom/transform-symbolic.svg"
+notify-send "Deu certo!" "Os arquivos foram convertidos para MOV com sucesso!" --app-name="Transform" --icon="$HOME/.local/share/icons/custom/transform-symbolic.svg"
 
 }
+
+exit 0
