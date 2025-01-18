dde5c29c5d2638b426c3e12171cfb10176f0d4c9
Fernando <33039864+tuxslack@users.noreply.github.com>
2025-01-17 23:31:09 -0300

    Update Converter para MOV (ProRes - Mesmo).sh

--- Vídeos/Converter para MOV (ProRes - Mesmo).sh
+++ Vídeos/Converter para MOV (ProRes - Mesmo).sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Colaboração:    Fernando Souza - https://www.youtube.com/@fernandosuporte/
+
+clear
+
+# -------------------------------------------------------------------------------------------------
+
+# Verificar se os programas estão instalados:
+
+which sed         1> /dev/null || exit 1
+which ffmpeg      1> /dev/null || exit 2
+which notify-send 1> /dev/null || exit 3
+which tr          1> /dev/null || exit 4
+
+
+# -------------------------------------------------------------------------------------------------
+
 {
 
 readarray FILENAME <<< "$(echo -e "$NAUTILUS_SCRIPT_SELECTED_FILE_PATHS" | sed -e 's/\r//g')"
@@ -9,6 +25,6 @@ for file in "${FILENAME[@]}"; do
     ffmpeg -i "$file" -vcodec prores_ks -qscale 0 -map 0 -acodec pcm_s16le -ac 1 -ar 22050 "${file%.*}-converted.mov"
 done
 
-notify-send "Deu certo!" "Os arquivos foram convertidos para MOV com sucesso!" --app-name="Transform" --icon="/home/raulcraveiro/.local/share/icons/custom/transform-symbolic.svg"
+notify-send "Deu certo!" "Os arquivos foram convertidos para MOV com sucesso!" --app-name="Transform" --icon="$HOME/.local/share/icons/custom/transform-symbolic.svg"
 
 }
